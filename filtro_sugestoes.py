47b0062850c78e2b785e264125310f06350061a6
Clara <clara.helena.souza@gmail.com>
2025-05-21 23:31:45 -0300

    Adicionado Regiões ao Filtro

--- filtro_sugestoes.py
+++ filtro_sugestoes.py
@@ -1,45 +1,49 @@
 '''
 Interesses provisorios do usuario logado.
 '''
-interesses_usuario = ['Rock', 'Pop']
+interesses_usuario = ['rock', 'pop', 'vinil']
+regiao_usuario = ['recife']
 
 '''
 lista de dicionario teste, onde cada dicionario é um estabelecimento
 '''
 estabelecimentos = [
-    {'nome': 'Labubu', 'interesses': ['Rock', 'Metal']},
-    {'nome': 'Taka', 'interesses': ['Pagode', 'Samba']},
-    {'nome': 'MusicBox', 'interesses': ['Pop', 'Eletrônica']},
-    {'nome': 'Casa da Música', 'interesses': ['Rock', 'Pop', 'MPB']},
+    {'nome': 'Labubu', 'interesses': ['Rock', 'Metal'], 'regiao': 'recife'},
+    {'nome': 'Taka', 'interesses': ['Pagode', 'Samba'], 'regiao': 'paulista'},
+    {'nome': 'MusicBox', 'interesses': ['Pop', 'Eletrônica'], 'regiao': 'olinda'},
+    {'nome': 'Casa da Música', 'interesses': ['Rock', 'Pop', 'MPB'], 'regiao': 'recife'},
 ]
 
 
-'''
-Função onde o 1 argumento é os interesses do usuario, e o 2 argumento é a lista de dicionarios dos estabelecimentos
-'''
-def recomendar_estabelecimentos(usuario_interesses, estabelecimentos):
+def recomendar_estabelecimentos(usuario_interesses, usuario_regiao, estabelecimentos):
     #Lista onde vai ficar os matchs do usuario com os estabelecimentos
     recomendados = []
 
+    usuario_interesses = [interesse.lower() for interesse in usuario_interesses]
+    usuario_regiao = usuario_regiao[0].lower()
+
     #for para separar os estabelecimentos
     for estabelecimento in estabelecimentos:
-        '''
-        #transforma a lista de interesses em set(conjuntos), pra poder usar &. 
-        # Se tiver interesses em comuns entre o usuario e os estabelecimentos, ele vai adicionar em recomendados o nome do estabelecimento e os interesses em comum.
-        '''
-        interesses_em_comum = set(usuario_interesses) & set(estabelecimento['interesses'])
-        if interesses_em_comum:
-            recomendados.append({
-                'nome': estabelecimento['nome'],
-                'interesses_em_comum': list(interesses_em_comum)
-            })
+
+        regiao_estabelecimento = estabelecimento['regiao'].lower()
+        interesses_estabelecimento = [i.lower() for i in estabelecimento['interesses']]
+
+
+        if regiao_estabelecimento == usuario_regiao:
+            interesses_em_comum = set(usuario_interesses) & set(interesses_estabelecimento)
+            if interesses_em_comum:
+                recomendados.append({
+                    'nome': estabelecimento['nome'],
+                    'interesses_em_comum': list(interesses_em_comum),
+                    'regiao': regiao_estabelecimento
+                })
     return recomendados
 
 #Chamando a função e inserindo os dados
-recomendacoes = recomendar_estabelecimentos(interesses_usuario, estabelecimentos)
+recomendacoes = recomendar_estabelecimentos(interesses_usuario, regiao_usuario, estabelecimentos)
 
 print("Estabelecimentos recomendados com base em seus interesses: ")
 #Printar o nome e o interesses para cada item da lista (recomendacoes)
 for rec in recomendacoes:
-    print(f"{rec['nome']} - Interesses em comum: {', '.join(rec['interesses_em_comum'])}")
-    #metodo .join pra juntar todos os elementos em uma unica string e separar eles com virgula
+    print(f"{rec['nome']} - Interesses em comum: {', '.join(rec['interesses_em_comum'])} - Região: {rec['regiao']}")
+ 
